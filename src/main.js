ad0af1a99e62dcad5bda0cffac9ec9a747502f57
Wang Liao <wliao7@ucsc.edu>
2023-05-01 15:00:29 -0700

    main.js information update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+/*
+
+Name: Wang Liao
+Mod Title: Rocket Patrol With Mod
+Approximate time: 12 hours
+
+Mod List:
+1. 5 Points
+    - Track a high score that persists across scenes and display it in the UI
+    - Implement the 'FIRE' UI text from the original game
+        - For this mod, I put the "FIRE" text slightly under the "Score" text becuase the top screen space has been occupied by other UIs
+    - Implement the speed increase that happens after 30 seconds in the original game
+    - Create a new scrolling tile sprite for the background
+    - Allow the player to control the Rocket after it's fired
+2. (10 Points)
+    - Create 4 new explosion sound effects and randomize which one plays on impact
+    - Display the time remaining (in seconds) on the screen
+    - Create a new title screen (e.g., new artwork, typography, layout)
+3. (15 Points)
+    - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
+    - Implement a new timing/scoring mechanism that adds time to the clock for successful hits
+    - Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship
+
+*/
+
 let config = 
 {
     type: Phaser.AUTO,
